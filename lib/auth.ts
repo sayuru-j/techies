6ce63885086eeb3da471cc8251a6c0181476cfa7
sayuru-j <zayuru.ai@gmail.com>
2023-07-07 14:33:42 +0530

    Updated NextAuth Url

--- lib/auth.ts
+++ lib/auth.ts
@@ -68,7 +68,7 @@ export const authOptions: NextAuthOptions = {
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
 
-        const res = await fetch("http://localhost:3000/api/auth/login", {
+        const res = await fetch(process.env.NEXTAUTH_URL + "/api/auth/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
